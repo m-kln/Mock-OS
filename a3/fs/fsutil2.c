077484bc7603d79676bc1ad03b7fb36128a4c594
Mona <mona.kalaoun@mail.mcgill.ca>
2024-03-31 17:21:31 -0400

    copy in

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -16,11 +16,48 @@
 
 int copy_in(char *fname) {
   // TODO
+  //real HD -> shell HD
+  //lack of free space -> "Warning: could only write %d out of %ld bytes (reached end of file)"
+  //%d: nbr of bytes you could write %ld:total file size of the file
+  
+  size_t b;
+  FILE* file = fopen(fname, "rb");
+
+  if (file == NULL) {
+    printf("fdn\n");
+    return -1;
+  }
+
+  //Find real HD file's size 
+  fseek(file, 0, SEEK_END); //point to the end of the file
+  unsigned int size = ftell(file); //calculate the size of the file
+  printf("size of OG file: %d\n", size);
+  fseek(file, 0, SEEK_SET); //point to start of file for reading
+
+  fsutil_create(fname, size); //create file on shell HD using same name and size as OG
+  
+  char* buffer = malloc((size+1)*sizeof(char)); 
+  //memset(buffer, 0 , size+1);
+  //while ((b=fread(buffer, 1, sizeof(buffer), file)) > 0){
+   // fsutil_write(fname, buffer, b);
+  //}
+  while (!feof(file)) {
+    fread(buffer, sizeof(buffer), sizeof(buffer), file);
+  }
+
+  fsutil_write(fname, buffer, size+1);
+
+  fclose(file);
+
+  //fsutil_seek(fname, offset); //reset file's offset
   return 0;
 }
 
 int copy_out(char *fname) {
   //code from lab 
+  struct file *file_s = get_file_by_fname(fname);
+  offset_t offset = file_tell(file_s); //store file's current offset 
+
   int size = fsutil_size(fname); //retrieve size of the file
   char* buffer = malloc((size+1)*sizeof(char)); 
   memset(buffer, 0 , size+1);
@@ -28,12 +65,15 @@ int copy_out(char *fname) {
   fsutil_read(fname, buffer, size);
 
   FILE* file = fopen(fname, "w");
+
   if (file == NULL) {
     printf("fdn\n");
     return -1;
   }
   fputs(buffer, file);
   fclose(file);
+
+  fsutil_seek(fname, offset); //reset file's offset
   return 0;
 }
 
