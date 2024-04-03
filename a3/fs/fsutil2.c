18d630cea2e36d2c981f27e942e34d7349e3013f
Mona <mona.kalaoun@mail.mcgill.ca>
2024-04-03 02:07:03 -0400

    pls hbb test 11

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -351,7 +351,7 @@ void recover(int flag) {
             //Create file in real filesystem
             FILE *file = fopen(filename, "wb"); //write in binary mode
             if (file != NULL){
-              fwrite(&buffer[start], 1, size, file); //write the data stored in buffer to the file
+              fwrite(&buffer[start+1], 1, size, file); //write the data stored in buffer to the file
               fclose(file);
             }
           }
