cf4795099d89c59fcc9116111c37d8a40f674102
Mona <mona.kalaoun@mail.mcgill.ca>
2024-04-03 00:36:43 -0400

    bjr

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -269,11 +269,9 @@ void recover(int flag) {
       buffer_cache_read(bit, buffer); //read contents of the sector represented by the current bit
 
       bool is_nonzero = false;
-      int temp = 0;
       for (size_t i = 0; i<512; i++){
         if (buffer[i] != 0) {
           is_nonzero = true;
-          temp = i;
           break;
         }
       }
@@ -283,7 +281,7 @@ void recover(int flag) {
         snprintf(name, sizeof(name), "recovered1-%ld.txt", bit);  //format filename
         FILE *file = fopen(name, "wb");
         if (file != NULL){
-          fwrite(buffer, 1, temp, file);
+          fwrite(buffer, 1, BLOCK_SECTOR_SIZE, file);
           fclose(file);
         }
       }
